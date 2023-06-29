f1b14778ecc081f43469bd213b34ff264e3e026a
mavargasa <monikavargas@gmail.com>
2023-06-28 20:13:31 -0500

    Actualizacion

--- main.js
+++ main.js
@@ -1,46 +1,76 @@
 $(document).ready(function() {
+  const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
   const $pokemonList = $('#pokemonList');
+  const $pokeName = $('#pokeName');
+  const $searchPokemon = $('#searchPokemon');
+  const $borrarPokemon = $('#borrarPokemon');
+  const $app = $('#app');
+  const displayedPokemons = []; // Array para almacenar los Pokémon mostrados en pantalla
 
-  function renderPokemonList(pokemonList) {
-    $pokemonList.empty();
+  $searchPokemon.click(insertarPokemon);
+  $borrarPokemon.click(borrarPokemon);
 
-    pokemonList.forEach(function(pokemon) {
-      const $card = $('<div class="col-md-4 mb-4">');
-      const $cardBody = $('<div class="card-body">');
-      const $cardTitle = $('<h5 class="card-title">').text(pokemon.name);
-      const $cardText = $('<p class="card-text">').text('Tipo: ' + pokemon.type.join(', '));
-      const $cardLink = $('<a href="#" class="card-link">').text('Ver detalles');
+  function insertarPokemon() {
+    const pokemonName = $pokeName.val().toLowerCase();
 
-      $cardLink.click(function() {
-        showDetails(pokemon);
+    // Comprobar si el Pokémon ya está en pantalla
+    if (displayedPokemons.includes(pokemonName)) {
+      alert('Este Pokémon ya está en pantalla');
+      return;
+    }
+
+    $.getJSON(`${baseURL}${pokemonName}`)
+      .done(function(data) {
+        const $card = $('<div class="col-md-4 mb-4">');
+        const $cardBody = $('<div class="card-body">');
+        const $cardTitle = $('<h5 class="card-title">').text(data.name);
+        const $cardText = $('<p class="card-text">').text('Tipo: ' + data.types.map(type => type.type.name).join(', '));
+        const $cardLink = $('<a href="#" class="card-link">').text('Ver detalles');
+
+        $cardLink.click(function() {
+          showDetails(data);
+        });
+
+        $cardBody.append($cardTitle, $cardText, $cardLink);
+        $card.append($cardBody);
+        $pokemonList.append($card);
+
+        displayedPokemons.push(pokemonName); // Agregar el Pokémon al array de mostrados en pantalla
+      })
+      .fail(function() {
+        alert('Este Pokémon no está disponible');
       });
-
-      $cardBody.append($cardTitle, $cardText, $cardLink);
-      $card.append($cardBody);
-      $pokemonList.append($card);
-    });
   }
 
   function showDetails(pokemon) {
     $('#modalTitle').text(pokemon.name);
-    $('#modalImage').attr('src', pokemon.ThumbnailImage);
+    $('#modalImage').attr('src', pokemon.sprites.front_default);
     $('#modalWeight').text(pokemon.weight);
-    $('#modalType').text(pokemon.type.join(', '));
+    $('#modalType').text(pokemon.types.map(type => type.type.name).join(', '));
     $('#modalAbilities').empty();
     $('#modalWeaknesses').empty();
 
     pokemon.abilities.forEach(function(ability) {
-      $('#modalAbilities').append($('<li>').text(ability));
+      $('#modalAbilities').append($('<li>').text(ability.ability.name));
     });
 
-    pokemon.weakness.forEach(function(weakness) {
-      $('#modalWeaknesses').append($('<li>').text(weakness));
-    });
+    $.getJSON(`${baseURL}${pokemon.id}`)
+      .done(function(data) {
+        data.types.forEach(function(typeData) {
+          $.getJSON(typeData.type.url)
+            .done(function(typeInfo) {
+              typeInfo.damage_relations.double_damage_from.forEach(function(weakness) {
+                $('#modalWeaknesses').append($('<li>').text(weakness.name));
+              });
+            });
+        });
+      });
 
     $('#pokemonModal').modal('show');
   }
 
-  $.getJSON('data.json', function(data) {
-    renderPokemonList(data);
-  });
+  function borrarPokemon() {
+    $pokemonList.empty();
+    displayedPokemons.length = 0; // Vaciar el array de mostrados en pantalla
+  }
 });
